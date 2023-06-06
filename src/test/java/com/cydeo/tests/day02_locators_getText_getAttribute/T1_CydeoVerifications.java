6ba7f103e023347f0b28f547665c4a67a5d0c9c9
Ono <ogerel@gmail.com>
2023-06-05 19:49:53 -0700

    Updating.

--- src/test/java/com/cydeo/tests/day02_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day02_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -1,7 +1,6 @@
-package com.cydeo.tests.day2_locators_getText_getAttribute;
+package com.cydeo.tests.day02_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
