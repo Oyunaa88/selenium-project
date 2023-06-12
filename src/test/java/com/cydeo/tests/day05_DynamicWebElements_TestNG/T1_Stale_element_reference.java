c856ec14ba2f23fd9b273c7404e5812cd5ee2cd4
Ono <ogerel@gmail.com>
2023-06-11 20:29:08 -0700

    Stale element and Loging page updated.

--- src/test/java/com/cydeo/tests/day05_DynamicWebElements_TestNG/T1_Stale_element_reference.java
+++ src/test/java/com/cydeo/tests/day05_DynamicWebElements_TestNG/T1_Stale_element_reference.java
@@ -3,12 +3,13 @@ package com.cydeo.tests.day05_DynamicWebElements_TestNG;
 import com.cydeo.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class T1_Stale_element_reference {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //1. Open Chrome browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
@@ -21,9 +22,21 @@ public class T1_Stale_element_reference {
         addElementButton.click();
 
 //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@onclick='deleteElement()']"));
+        System.out.println("deleteButton.isDisplayed().expecting true= " + deleteButton.isDisplayed());
 //5. Click to “Delete” button.
+        Thread.sleep(2000);
+        deleteButton.click();
+
 //6. Verify “Delete” button is NOT displayed after clicking.
+        //use try/catch to handle and verify
 
-
+        try {
+            System.out.println("deleteButton.isDisplayed(), expecting false = " + deleteButton.isDisplayed());
+        }catch(StaleElementReferenceException e){
+            System.out.println("-->StaleElementReferenceException happened due to element being deleted from the page.");
+            System.out.println("Which concludes our test case -PASSING!-");
+            System.out.println("deleteButton.isDisplayed()= false ");
+        }
     }
 }
